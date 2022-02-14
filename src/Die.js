dcd27c438d21dc29cf51b2033700a2c3e84e42e3
unknown <karthiku19@gmail.com>
2022-02-14 10:33:04 +0530

    initial commit

---
+++ src/Die.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./Die.css";
+
+class Die extends Component {
+  handleClick = () => {
+    this.props.handleClick(this.props.idx)
+  }
+
+  render() {
+    return (
+      <button
+        className={"Die"}
+        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
+        onClick={this.handleClick}
+      >
+        {this.props.val}
+      </button>
+    );
+  }
+}
+
+export default Die;
---
+++ src/RuleRow.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './RuleRow.css'
+
+class RuleRow extends Component {
+  render() {
+    const {score, name, doScore } = this.props
+    return (
+      <tr className={`RuleRow RuleRow-${score === undefined ? 'active' : 'disabled'}`} onClick={score === undefined ? doScore : null}>
+        <td className="RuleRow-name">{name}</td>
+        <td className="RuleRow-score">{score}</td>
+      </tr>
+    )
+  }
+}
+
+export default RuleRow;
